f763b0e49ec6bba3cbb81c79cd821b8c49972ff2
Yuu Fuerugen <furugen098@gmail.com>
2019-04-04 17:25:21 +0900

    サンプルコミット

---
+++ wpf-textbox-with-options/wpf-textbox-with-options/Data/SampleData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_textbox_width_options.Data
+{
+    public class SampleData
+    {
+        public string Name { get; set; }
+        public string Hiragana { get; set; }
+        public string Romaji { get; set; }
+    }
+}
